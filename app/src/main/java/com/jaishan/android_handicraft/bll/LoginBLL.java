956ba209cfe5c3ab31ab975f176599abf58d87ff
jaishan <jaisangs101@gmail.com>
2020-02-23 12:34:00 +0545

    55

---
+++ app/src/main/java/com/jaishan/android_handicraft/bll/LoginBLL.java
@@ -0,0 +1,31 @@
+package com.jaishan.android_handicraft.bll;
+
+import com.jaishan.handicraftstore.api.UserAPI;
+import com.jaishan.handicraftstore.server_response.SignUpResponse;
+import com.jaishan.handicraftstore.url.URL;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class LoginBLL {
+    boolean isSuccessful = false;
+
+    public boolean checkUser(String phone, String password) {
+
+        UserAPI usersAPI = URL.getInstance().create(UserAPI.class);
+        Call<SignUpResponse> usersCall = usersAPI.checkUser(phone, password);
+
+        try {
+            Response<SignUpResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful()) {
+                URL.token += loginResponse.body().getToken();
+                isSuccessful = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccessful;
+    }
+}
