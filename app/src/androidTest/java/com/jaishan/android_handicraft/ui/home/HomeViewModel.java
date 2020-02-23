4ed8a5ad707d6dc28bc13bfd4ee810920a36553c
jaishan <jaisangs101@gmail.com>
2020-02-23 12:18:17 +0545

    31

--- app/src/androidTest/java/com/jaishan/android_handicraft/ui/home/HomeViewModel.java
+++ app/src/androidTest/java/com/jaishan/android_handicraft/ui/home/HomeViewModel.java
@@ -1,4 +1,19 @@
-package com.jaishan.android_handicraft.ui.home;
+package com.jaishan.handicraftstore.ui.home;
 
-public class HomeViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
