2bf18e1ec1dc7413eb63f992bbc38ede6f8706e9
jaishan <jaisangs101@gmail.com>
2020-02-23 12:08:01 +0545

    3master

--- app/src/androidTest/java/com/jaishan/android_handicraft/strictmode/StrictMode.java
+++ app/src/androidTest/java/com/jaishan/android_handicraft/strictmode/StrictMode.java
@@ -1,4 +1,11 @@
-package com.jaishan.android_handicraft.strictmode;
+package com.jaishan.handicraftstore.strictmode;
 
 public class StrictMode {
+    public static void StrictMode() {
+        android.os.StrictMode.ThreadPolicy policy =
+                new android.os.StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build();
+
+        android.os.StrictMode.setThreadPolicy(policy);
+    }
 }
