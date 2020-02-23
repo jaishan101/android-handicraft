b431918509556898c2fcb84f96a0680a5e3ac9fb
jaishan <jaisangs101@gmail.com>
2020-02-23 12:15:59 +0545

    27

--- app/src/androidTest/java/com/jaishan/android_handicraft/ui/account/AccountFragement.java
+++ app/src/androidTest/java/com/jaishan/android_handicraft/ui/account/AccountFragement.java
@@ -1,4 +1,40 @@
-package com.jaishan.android_handicraft.ui.account;
+package com.jaishan.handicraftstore.ui.account;
 
-public class AccountFragement {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.jaishan.handicraftstore.LoginRegisterActivity;
+import com.jaishan.handicraftstore.R;
+
+public class AccountFragment extends Fragment {
+
+    private AccountViewModel accountViewModel;
+    private Button btnOpenLoginSignupFragment;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        accountViewModel =
+                ViewModelProviders.of(this).get(AccountViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_account, container, false);
+        //final TextView textView = root.findViewById(R.id.text_account);
+        btnOpenLoginSignupFragment = root.findViewById(R.id.btnOpenLoginSignupFragment);
+
+        btnOpenLoginSignupFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(getActivity(), LoginRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
 }
