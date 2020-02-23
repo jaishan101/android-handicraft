91543b352631a5fe3dcf9a5252cea1d66ae82fb3
jaishan <jaisangs101@gmail.com>
2020-02-23 12:07:29 +0545

    2master

--- app/src/androidTest/java/com/jaishan/android_handicraft/server_response/SignUpResponse.java
+++ app/src/androidTest/java/com/jaishan/android_handicraft/server_response/SignUpResponse.java
@@ -1,4 +1,27 @@
-package com.jaishan.android_handicraft.server_response;
+package com.jaishan.handicraftstore.server_response;
 
 public class SignUpResponse {
+    private String status;
+    private String token;
+
+    public SignUpResponse(String status, String token) {
+        this.status = status;
+        this.token = token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
