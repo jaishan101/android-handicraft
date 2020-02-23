9941154217dcb53c01a6cac07134b64bfb39cca3
jaishan <jaisangs101@gmail.com>
2020-02-23 12:10:05 +0545

    7master

--- app/src/androidTest/java/com/jaishan/android_handicraft/ui/RegisterActivity.java
+++ app/src/androidTest/java/com/jaishan/android_handicraft/ui/RegisterActivity.java
@@ -1,4 +1,114 @@
-package com.jaishan.android_handicraft.ui;
+package com.jaishan.handicraftstore.ui;
 
-public class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jaishan.handicraftstore.MainActivity;
+import com.jaishan.handicraftstore.R;
+import com.jaishan.handicraftstore.api.UserAPI;
+import com.jaishan.handicraftstore.model.User;
+import com.jaishan.handicraftstore.server_response.SignUpResponse;
+import com.jaishan.handicraftstore.url.URL;
+
+import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView login_text;
+    private EditText etName, etEmail, etPhoneNumber, etSMSCode, etPasswordRegister;
+    private Button btnSendSMS, btnSignUp;
+
+    Random r = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+        etSMSCode = findViewById(R.id.etSMSCode);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPasswordRegister = findViewById(R.id.etPasswordRegister);
+        btnSendSMS = findViewById(R.id.btnSendSMS);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        login_text = findViewById(R.id.login_text);
+
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etPhoneNumber.getText().toString())){
+                    etPhoneNumber.setError("Enter Phone Number");
+                    return;
+                }
+                final int random = (int)(Math.random() * 9999 + 0000);
+                etSMSCode.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        etSMSCode.setText(Integer.toString(random));
+                    }
+                }, 3000);
+            }
+        });
+
+        login_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(RegisterActivity.this, com.jaishan.handicraftstore.ui.LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = etPhoneNumber.getText().toString();
+                String password = etPasswordRegister.getText().toString();
+                String name = etName.getText().toString();
+                String email = etEmail.getText().toString();
+
+                User users = new User(name, email, password, phone);
+
+                UserAPI usersAPI = URL.getInstance().create(UserAPI.class);
+                Call<SignUpResponse> signUpCall = usersAPI.registerUser(users);
+
+                signUpCall.enqueue(new Callback<SignUpResponse>() {
+                    @Override
+                    public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                        if (!response.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Code " + response.code(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Toast.makeText(RegisterActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                        Intent intent =  new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onFailure(Call<SignUpResponse> call, Throwable t) {
+                        Toast.makeText(RegisterActivity.this, "Register Unsuccessful" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
 }
